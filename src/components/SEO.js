455a31e333abf1ca5a5b744bd41ce8900da4f4df
Daniel <arangomonsalve.daniel@gmail.com>
2020-06-24 22:11:30 -0500

    Facebook Card

--- src/components/SEO.js
+++ src/components/SEO.js
@@ -24,6 +24,14 @@ const SEO = ({ title, description }) => {
     <Helmet htmlAttributes={{ lang: "es" }} title={`${title} | ${siteTitle}`}>
       <meta name="description" content={description || siteDescription}/>
       <meta name="image" content={image}/>
+      {/*Facebook Card*/}
+      <meta property="og:url" content={siteURL}/>
+      <meta property="og:type" content="website"/>
+      <meta property="og:title" content={siteTitle}/>
+      <meta property="og:description" content={siteDescription}/>
+      <meta property="og:image" content={`${siteURL}${image}`}/>
+      <meta property="og:image:width" content="400"/>
+      <meta property="og:image:height" content="300"/>
       {/*Twitter Card*/}
       <meta name="twitter:card" content="summary_large_image"/>
       <meta name="twitter:creator" content={twitterUsername}/>
